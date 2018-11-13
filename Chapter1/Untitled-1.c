de251994f3bce7e9942c4bb68803d3b2d191396b
LiFanjieSeizeTheDay <lifanjiemail@163.com>
2018-11-14 01:10:02 +0800

    Update Chapter1/Untitled-1.c

---
+++ Chapter1/Untitled-1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <time.h>
+
+void PrintN(int N);
+void PrintN_1(int N);
+
+clock_t start, stop;
+double duration1, duration2;
+
+int main(void)
+{
+    int ii = 32767;
+
+    start = clock();
+    PrintN(ii);
+    stop = clock();
+    duration1 = ((double) (stop - start)) / CLK_TCK;
+    
+    start = clock();
+    PrintN_1(ii);
+    stop = clock();
+    duration2 = ((double) (stop - start)) / CLK_TCK;
+    
+    printf("%.2f\n", duration1);
+    printf("%.2f\n", duration2);
+
+
+    return 0;
+}
+
+void PrintN(int N)
+{
+    int i;
+    for(i = 1; i <= N; i++)
+    {
+        printf("%d\n", i);
+    }
+    return ;
+}
+
+void PrintN_1(int N)
+{
+    if(N)
+    {
+        PrintN(N - 1);
+        printf("%d\n", N);
+    }
+    return ;
+}
